df1f45f1f4f37d74396b22eeba7349feefbe9104
Jacob Reinhold <jacob.reinhold@jhu.edu>
2020-12-12 14:19:56 -0500

    fixes to flow

--- counterfactualms/experiments/calabresi/conditional_flow.py
+++ counterfactualms/experiments/calabresi/conditional_flow.py
@@ -1,5 +1,5 @@
 import pyro
-from pyro.nn import pyro_method, DenseNN
+from pyro.nn import pyro_method
 from pyro.distributions import (
     Normal, Bernoulli, Uniform, TransformedDistribution, MixtureOfDiagNormalsSharedCovariance  # noqa: F401
 )
@@ -19,42 +19,40 @@ class ConditionalFlowVISEM(BaseVISEM):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        # brain_volume flow
         self.brain_volume_flow_components = conditional_spline(1, 2, [8, 16])
-        self.brain_volume_flow_transforms = nn.ModuleList([
+        self.brain_volume_flow_transforms = [
             self.brain_volume_flow_components, self.brain_volume_flow_constraint_transforms
-        ])
+        ]
 
-        # ventricle_volume flow
         self.ventricle_volume_flow_components = conditional_spline(1, 2, [8, 16])
-        self.ventricle_volume_flow_transforms = nn.ModuleList([
+        self.ventricle_volume_flow_transforms = [
             self.ventricle_volume_flow_components, self.ventricle_volume_flow_constraint_transforms
-        ])
+        ]
 
-        # lesion_volume flow
         self.lesion_volume_flow_components = conditional_spline(1, 4, [16, 32])
-        self.lesion_volume_flow_transforms = nn.ModuleList([
+        self.lesion_volume_flow_transforms = [
             self.lesion_volume_flow_components, self.lesion_volume_flow_constraint_transforms
-        ])
+        ]
 
-        # duration flow
         self.duration_flow_components = conditional_spline(1, 2, [8, 16])
-        self.duration_flow_transforms = nn.ModuleList([
+        self.duration_flow_transforms = [
             self.duration_flow_components, self.duration_flow_constraint_transforms
-        ])
+        ]
 
-        # score flow
         self.score_flow_components = conditional_spline(1, 3, [10, 20])
-        self.score_flow_transforms = nn.ModuleList([
+        self.score_flow_transforms = [
             self.score_flow_components, self.score_flow_constraint_transforms
-        ])
+        ]
 
-        self.prior_flow = nn.ModuleList([
-            conditional_spline_autoregressive(self.latent_dim, 3, [128, 128], order='quadratic')
-        ])
-        self.posterior_flow = nn.ModuleList([
-            conditional_spline_autoregressive(self.latent_dim, 3, [128, 128], order='quadratic')
-        ])
+        self.prior_flow_components = conditional_spline_autoregressive(self.latent_dim, 3, [128, 128], order='quadratic')
+        self.prior_flow_transforms = [
+            self.prior_flow_components
+        ]
+
+        self.posterior_flow_components = conditional_spline_autoregressive(self.latent_dim, 3, [128, 128], order='quadratic')
+        self.posterior_flow_transforms = [
+            self.posterior_flow_components
+        ]
 
     @pyro_method
     def pgm_model(self):
@@ -65,15 +63,15 @@ class ConditionalFlowVISEM(BaseVISEM):
 
         age_base_dist = Normal(self.age_base_loc, self.age_base_scale).to_event(1)
         age_dist = TransformedDistribution(age_base_dist, self.age_flow_transforms)
-        age = pyro.sample('age', age_dist)
         _ = self.age_flow_components
+        age = pyro.sample('age', age_dist)
         age_ = self.age_flow_constraint_transforms.inv(age)
 
         brain_context = torch.cat([sex, age_], 1)
         brain_volume_base_dist = Normal(self.brain_volume_base_loc, self.brain_volume_base_scale).to_event(1)
         brain_volume_dist = ConditionalTransformedDistribution(brain_volume_base_dist, self.brain_volume_flow_transforms).condition(brain_context)
-        brain_volume = pyro.sample('brain_volume', brain_volume_dist)
         _ = self.brain_volume_flow_components
+        brain_volume = pyro.sample('brain_volume', brain_volume_dist)
         brain_volume_ = self.brain_volume_flow_constraint_transforms.inv(brain_volume)
 
         duration_context = torch.cat([sex, age_], 1)
@@ -117,7 +115,8 @@ class ConditionalFlowVISEM(BaseVISEM):
         ctx = torch.cat([ventricle_volume_, brain_volume_, lesion_volume_], 1)
 
         z_base_dist = Normal(self.z_loc, self.z_scale).to_event(1)
-        z_dist = ConditionalTransformedDistribution(z_base_dist, self.prior_flow).condition(ctx)
+        z_dist = ConditionalTransformedDistribution(z_base_dist, self.prior_flow_transforms).condition(ctx)
+        _ = self.prior_flow_components
         with poutine.scale(scale=self.annealing_factor):
             z = pyro.sample('z', z_dist)
         latent = torch.cat([z, ventricle_volume_, brain_volume_, lesion_volume_], 1)
@@ -141,7 +140,8 @@ class ConditionalFlowVISEM(BaseVISEM):
             ctx = torch.cat([ventricle_volume_, brain_volume_, lesion_volume_], 1)
 
             latent_base_dist = self.latent_encoder.predict(hidden)
-            latent_dist = ConditionalTransformedDistribution(latent_base_dist, self.posterior_flow).condition(ctx)
+            latent_dist = ConditionalTransformedDistribution(latent_base_dist, self.posterior_flow_transforms).condition(ctx)
+            _ = self.posterior_flow_components
             with poutine.scale(scale=self.annealing_factor):
                 z = pyro.sample('z', latent_dist)
 
